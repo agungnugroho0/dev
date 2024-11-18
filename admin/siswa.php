922a095d1bb413c29b94c3b0bd8408bbb89005f8
agungnugroho0 <agungwae205@gmail.com>
2024-11-18 11:33:17 +0700

    tambah tailwind, kelas di siswa dan ganti gambar

--- admin/siswa.php
+++ admin/siswa.php
@@ -16,7 +16,34 @@ require '../config/admin.php';
 
 <body>
     <?php include 'menu.html'; ?>
+    <div class="bg-slate-50 grid grid-cols-1 lg:grid-cols-4 ">
+        <?php
+            $kelas = $konek ->query("SELECT * FROM kelas");
+            $data_kelas_array = [];
+            while($data_kelas = $kelas ->fetch_assoc()) {
+                $data_kelas_array[] = $data_kelas;?>
+                <div class="bg-white shadow-md p-3 m-3 rounded-lg">
+                    <p class="font-bold">KELAS <?= $data_kelas['kelas']?></p>
+                    <hr>
+                    <p><?php  $wali = $konek ->query("SELECT nama FROM staff WHERE id_kelas = $data_kelas[id_kelas]") ->fetch_assoc(); if($wali){echo "Wali : ".$wali['nama'];}else {echo "";} ?></p>
 
+                </div>
+
+            <?php 
+            }?>
+        
+        <div class="bg-white shadow-md p-3 m-3 rounded-lg lg:col-span-2">Daftar Kelas
+            <?php
+             foreach ($data_kelas_array as $data_kelas) { ?>
+                <div class="flex"><?=$data_kelas['kelas']?>
+                <a href="#"><img src="../image/pensil.png"> &nbsp;edit</a>
+                <a href="#"> &nbsp;hapus</a>
+                </div>
+            <?php
+            }
+            ?>
+        </div>
+    </div>
 </body>
 
 </html>
